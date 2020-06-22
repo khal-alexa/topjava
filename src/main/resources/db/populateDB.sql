86cd3a6e804a3b5f2b2249aa10896e3f1e48c2c9
khal-alexa <oleksandra.khalaim@gmail.com>
2020-06-22 21:03:05 +0300

    HW03 is done

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,11 +1,19 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+DELETE
+FROM user_roles;
+DELETE
+FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
-INSERT INTO users (name, email, password) VALUES
-  ('User', 'user@yandex.ru', 'password'),
-  ('Admin', 'admin@gmail.com', 'admin');
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password'),
+       ('Admin', 'admin@gmail.com', 'admin');
 
-INSERT INTO user_roles (role, user_id) VALUES
-  ('ROLE_USER', 100000),
-  ('ROLE_ADMIN', 100001);
+INSERT INTO user_roles (role, user_id)
+VALUES ('ROLE_USER', 100000),
+       ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (description, calories, user_id)
+VALUES ('dinner', 500, 100000),
+       ('breakfast', 750, 100001),
+       ('dinner2', 850, 100001);
+
